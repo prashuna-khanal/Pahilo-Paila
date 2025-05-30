d9a7afbc26decedc4931009ea8df6d2f0b91876a
Mibish <mibishghimire798@gmail.com>
2025-05-30 21:15:02 +0545

    Database

---
+++ src/pahilopaila/database/dbconnection.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pahilopaila.database;
+import java.sql.*;
+
+/**
+ *
+ * @author Mibish
+ */
+public interface dbconnection {
+    Connection openConnection();
+    void closeConnection(Connection conn);
+    ResultSet runQuery(Connection conn,String query);
+    int executeUpdate(Connection conn,String query);
+    
+    
+}
