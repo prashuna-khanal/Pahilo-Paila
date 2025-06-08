0c49a9035e14ad0093736cacce2c5a62614ab5e0
Mibish <mibishghimire798@gmail.com>
2025-06-08 09:43:09 +0545

    Delete build directory

--- build/classes/pahilopaila/view/UserInterface.rs
+++
@@ -1,7 +0,0 @@
-pahilopaila.UserInterface$6
-pahilopaila.UserInterface$5
-pahilopaila.UserInterface$4
-pahilopaila.UserInterface$3
-pahilopaila.UserInterface$2
-pahilopaila.UserInterface
-pahilopaila.UserInterface$1
