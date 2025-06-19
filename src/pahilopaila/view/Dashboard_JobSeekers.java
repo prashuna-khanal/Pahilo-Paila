cc0c71a554363aa204fa9ddf0f8e9540adbffc5c
justprabesh <prabesh.mahatara24@gmail.com>
2025-06-19 17:15:48 +0545

    Job Seekers settings update.

--- src/pahilopaila/view/Dashboard_JobSeekers.java
+++ src/pahilopaila/view/Dashboard_JobSeekers.java
@@ -8,7 +8,7 @@ import java.util.TimerTask;
 import pahilopaila.Controller.Dashboard_JobseekersController;
 
 /**
- * Job Seeker Dashboard UI for the Pahilo Paila application.
+ * Job Seeker Dashboard UI for the PahiloPaila application.
  */
 public class Dashboard_JobSeekers extends javax.swing.JFrame {
     // State variables for label hover and pressed states
